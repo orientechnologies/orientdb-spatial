1ccce06221844400d2d59bdfd08e8b56b1474866
Roberto Franchini <ro.franchini@gmail.com>
2017-03-23 17:09:43 +0100

    fixups spatial tests

--- distribution/src/test/java/com/orientechnologies/distribution/integration/OSpatialQaIT.java
+++ distribution/src/test/java/com/orientechnologies/distribution/integration/OSpatialQaIT.java
@@ -1,11 +1,7 @@
 package com.orientechnologies.distribution.integration;
 
-import com.orientechnologies.orient.core.record.impl.ODocument;
 import com.orientechnologies.orient.core.sql.executor.OResultSet;
-import com.orientechnologies.orient.core.sql.query.OSQLSynchQuery;
 import org.junit.Test;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,16 +12,13 @@ public class OSpatialQaIT extends OIntegrationTestTemplate {
 
   @Test
   public void testSpatial() throws Exception {
+    OResultSet resultSet;
 
-//    OResultSet resultSet = db.query(
-//        "select * from ArchaeologicalSites where  ST_WITHIN(location,ST_Buffer(ST_GeomFromText('POINT(13.4115753 52.539275 )'), 1)) = true");
+    resultSet = db.query(
+        "SELECT from ArchaeologicalSites where ST_Within(Location, ST_Buffer(ST_GeomFromText('POINT(41.8898464 12.4866727 )'), 50)) = true");
+
 //
-//    assertThat(resultSet).hasSize(1);
-
-    List<?> list = db.query(new OSQLSynchQuery<ODocument>(
-        "select * from ArchaeologicalSites where  ST_WITHIN(location,ST_Buffer(ST_GeomFromText('POINT(12.5113300 41.8919300  )'), 2)) = true"));
-
-    assertThat(list).hasSize(1);
+    assertThat(resultSet).hasSize(55);
 
   }
 }
