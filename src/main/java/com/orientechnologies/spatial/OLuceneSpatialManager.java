5132e5251105fa3ef67fb1a9818d10e6514ab50b
Tglman <tglman@tglman.com>
2018-04-05 13:40:23 +0100

    attempt to bypass distributed listener

--- src/main/java/com/orientechnologies/spatial/OLuceneSpatialManager.java
+++ src/main/java/com/orientechnologies/spatial/OLuceneSpatialManager.java
@@ -17,7 +17,11 @@
  */
 package com.orientechnologies.spatial;
 
+import com.orientechnologies.orient.core.command.OCommandExecutor;
+import com.orientechnologies.orient.core.command.OCommandRequestText;
+import com.orientechnologies.orient.core.db.ODatabase;
 import com.orientechnologies.orient.core.db.ODatabaseInternal;
+import com.orientechnologies.orient.core.db.ODatabaseListener;
 import com.orientechnologies.spatial.shape.OShapeBuilder;
 
 import static com.orientechnologies.spatial.shape.OShapeBuilder.BASE_CLASS;
@@ -35,6 +39,69 @@ public class OLuceneSpatialManager {
 
   public void init(ODatabaseInternal db) {
 
+    //This is just for bypass the distributed listener that conflict with create class
+    db.registerListener(new ODatabaseListener() {
+      @Override
+      public void onCreate(ODatabase iDatabase) {
+        internalInit((ODatabaseInternal) iDatabase);
+        iDatabase.unregisterListener(this);
+      }
+
+      @Override
+      public void onDelete(ODatabase iDatabase) {
+
+      }
+
+      @Override
+      public void onOpen(ODatabase iDatabase) {
+        internalInit((ODatabaseInternal) iDatabase);
+        iDatabase.unregisterListener(this);
+      }
+
+      @Override
+      public void onBeforeTxBegin(ODatabase iDatabase) {
+
+      }
+
+      @Override
+      public void onBeforeTxRollback(ODatabase iDatabase) {
+
+      }
+
+      @Override
+      public void onAfterTxRollback(ODatabase iDatabase) {
+
+      }
+
+      @Override
+      public void onBeforeTxCommit(ODatabase iDatabase) {
+
+      }
+
+      @Override
+      public void onAfterTxCommit(ODatabase iDatabase) {
+
+      }
+
+      @Override
+      public void onClose(ODatabase iDatabase) {
+
+      }
+
+      @Override
+      public void onBeforeCommand(OCommandRequestText iCommand, OCommandExecutor executor) {
+
+      }
+
+      @Override
+      public void onAfterCommand(OCommandRequestText iCommand, OCommandExecutor executor, Object result) {
+
+      }
+    });
+
+  }
+
+  private void internalInit(ODatabaseInternal db) {
     if (db.getMetadata().getSchema().getClass(BASE_CLASS) == null) {
       db.getMetadata().getSchema().createAbstractClass(BASE_CLASS);
       shapeBuilder.initClazz(db);
